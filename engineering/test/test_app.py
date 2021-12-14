55e929a5fa3eb96c72d08cc3d985168f030615ca
tyrnaki <walter.tyrna@gmail.com>
2021-12-14 17:47:31 -0500

    Update test_app.py

--- engineering/test/test_app.py
+++ engineering/test/test_app.py
@@ -1,19 +1,7 @@
 import streamlit as st
 import pandas as pd
-import pymongo
-from pymongo import MongoClient
-
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = pymongo.MongoClient("mongodb+srv://wtyrna13:WkuLFvYJ7rUHQXu@cluster0.tpw7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.new_scrapes
-cursor = db.reddit_data.find()
-most_recent = list(cursor)[-1]
-data = pd.DataFrame.from_dict(most_recent, orient='index')
-data.rename(columns={data.columns[0]: "text" }, inplace = True)
-data = data.reset_index()
-data = data.drop(['index'],axis=1)
 
 st.write(
 'This is a test'
 )
-st.pyplot(data)
+
