2c70e039ed60541697667451addaad407ac93379
tyrnaki <walter.tyrna@gmail.com>
2021-12-14 20:57:03 -0500

    Update test_app.py

--- engineering/test/test_app.py
+++ engineering/test/test_app.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = pymongo.MongoClient("mongodb+srv://wtyrna13:WkuLFvYJ7rUHQXu@cluster0.tpw7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",connect=False)
 db = client.new_scrapes
-db.collection_names()
+db.list_collection_names()
 cursor = db.reddit_data.find()
 most_recent = list(cursor)
 data = pd.DataFrame.from_dict(most_recent, orient='index')
