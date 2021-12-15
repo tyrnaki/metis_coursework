75fcfeed6a28a1eebaa5f602691d2cd31721bbf5
tyrnaki <walter.tyrna@gmail.com>
2021-12-14 20:36:07 -0500

    Update test_app.py

--- engineering/test/test_app.py
+++ engineering/test/test_app.py
@@ -4,7 +4,7 @@ import pymongo
 from pymongo import MongoClient
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = pymongo.MongoClient("mongodb+srv://wtyrna13:WkuLFvYJ7rUHQXu@cluster0.tpw7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://wtyrna13:WkuLFvYJ7rUHQXu@cluster0.tpw7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",connect=False)
 db = client.new_scrapes
 cursor = db.reddit_data.find()
 most_recent = list(cursor)[-1]
