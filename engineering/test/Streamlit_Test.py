7f3a13a6e242edcd40b589d5adb9b6a85a9b3e9a
tyrnaki <walter.tyrna@gmail.com>
2021-12-16 10:38:01 -0500

    Update Streamlit_Test.py

--- engineering/test/Streamlit_Test.py
+++ engineering/test/Streamlit_Test.py
@@ -35,6 +35,7 @@ db = client.for_class
 
 db.list_collection_names()
 
+
 # In[4]:
 
 
@@ -85,57 +86,62 @@ def drugs_counter(x):
 	df.rename(columns={ df.columns[0]: "drug" , df.columns[1]: "frequency" }, inplace = True)
 	return df.sort_values(by=['frequency'], ascending=False)
 
+def drug_definer(x):
+    drug_links_list = ""
+    for i in x:
+        link = 'https://www.google.com/search?q='+i
+        drug_string = '**['+i+']('+link+')**'
+        drug_links_list+drug_string
+    return drug_links_list
 
 # In[42]:
+st.set_page_config(layout="wide")
 
 st.title('Novel Psychoactive Substance Monitoring Dashboard')
 st.write('This dashboard is designed to provide insights into drug trends based off of the subreddit [Research Chemicals](https://www.reddit.com/r/researchchemicals/). Using a Natural Language Processing model, I categorize posts into categories; the charts below provide updated analysis of the text.')
 
-col1, col2 = st.columns((1,1))
+col1, col2, col3= st.columns((2,0.25,1))
 
 with col1:
-    
-    st.subheader('What are people talking about:')
-    st.selectbox(
-     'choose timeframe',
-     ('Today', 'Yesterday', 'This Week'))
-    
-    st.write('Updated as of x')
-    #st.dataframe(drugs_today)
-    
+	#cols1,_= st.columns((3,1)) # To make it narrower
+	st.subheader('Drug Reactions Over the Past Year:')
+	selection = st.selectbox(
+	     'choose reaction',
+	     ('Dangerous', 'Negative', 'Euphoric', 'Psycadelic'))
+
+	format = 'MMM DD, YYYY'  # format output
+	start_date = datetime.now().date()-relativedelta(years=1)  #  I need some range in the past
+	end_date = datetime.now().date()
+	max_days = end_date-start_date
+	        
+	slider = col1.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date, format=format)
+	        ## Sanity check
+	start_date_t = type(end_date)
+
+	selected_date = slider.strftime("%b %Y")
+
+	one_year_df = reaction_masker(selection, start_date, slider)
+	year_df = drugs_counter(one_year_df.drugs_in_text)
+
+	one_year_chart = px.bar(year_df.head(10), x='drug', y='frequency', title='Most Common Drug Terms Associated with '+ selection + ' Effects, from Dec 2020 to '+selected_date)
+	one_year_chart.update_layout(height=400)
+
+	st.plotly_chart(one_year_chart, height=400)
+
 with col2:
-    st.subheader('What are these drugs?')
+	st.text('')
 
-st.subheader('Drug Reactions Over the Past Year:')
-selection = st.selectbox(
-     'choose reaction',
-     ('Dangerous', 'Negative', 'Euphoric', 'Psycadelic'))
-	
+with col3:
+	st.subheader('What are these substances?')
+	st.markdown('The listed substances below correspond to the chart on the left.')
+	drug_links = drug_definer(year_df['drug'].head(10))
+	st.markdown(drug_links)
 
-cols1,_ = st.columns((3,1)) # To make it narrower
-format = 'MMM DD, YYYY'  # format output
-start_date = datetime.now().date()-relativedelta(years=1)  #  I need some range in the past
-end_date = datetime.now().date()
-max_days = end_date-start_date
-        
-slider = cols1.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date, format=format)
-        ## Sanity check
-start_date_t = type(end_date)
 
-selected_date = slider.strftime("%b %Y")
+st.components.v1.iframe('https://github.com/tyrnaki/metis_coursework/blob/main/engineering/test/lda.html', width=500, height=500)
 
-one_year_df = reaction_masker(selection, start_date, slider)
-year_df = drugs_counter(one_year_df.drugs_in_text)
-one_year_chart = px.bar(year_df.head(10), x='drug', y='frequency', title='Most Common Drug Terms Associated with '+ selection + ' Effects, from Dec 2020 to '+selected_date)
-one_year_chart.update_layout(height=400)
-
-st.plotly_chart(one_year_chart, height=400)
 
 # In[ ]:
-
-
-
-
 
 
 
