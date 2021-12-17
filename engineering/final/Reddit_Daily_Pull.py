c4a315ced53570c081f823d64a9a612b21363582
tyrnaki <walter.tyrna@gmail.com>
2021-12-17 10:21:30 -0500

    Add files via upload

---
+++ engineering/final/Reddit_Daily_Pull.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import praw
+import pymongo
+from pymongo import MongoClient
+import json
+import pandas as pd
+
+reddit = praw.Reddit(client_id='X6OHhwJW1MsEi9OIA_e_YA', client_secret='7J31PixC3u63zoNRFLquYSfRBLvlBQ', user_agent='WTyrna_Scrape')
+
+client = pymongo.MongoClient("mongodb+srv://wtyrna13:WkuLFvYJ7rUHQXu@cluster0.tpw7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
+db = client.new_scrapes
+
+posts = []
+ml_subreddit = reddit.subreddit('researchchemicals')
+for post in ml_subreddit.new(limit=100):
+    posts.append([post.id, post.selftext])
+posts = pd.DataFrame(posts,columns=['id', 'body'])
+
+submissions = []
+for post in posts.id:
+    submission = reddit.submission(id=post)
+    for top_level_comment in submission.comments:
+        submissions.append([post, top_level_comment.body])
+
+comments = pd.DataFrame(submissions, columns = ['id', 'body'])
+
+one_day_combined = pd.concat([posts, comments])
+
+one_day_combined_dict = one_day_combined.set_index('id')['body'].to_dict()
+
+result=db.reddit_data.insert_one(one_day_combined_dict)
+
+
+
+
+
+
+
+
